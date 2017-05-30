64e386335efc9f7c9cbd3319b04d2c4af167f281
Shivam Chawla <schawla5@sapient.com>
2017-05-30 12:25:06 +0530

    Redis Dotnet

---
+++ shreoyedis/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace shreoyedis
+{
+    internal class Person
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+}
--- shreoyedis/Program.cs
+++ shreoyedis/Program.cs
@@ -1,15 +1,41 @@
-﻿using System;
+﻿using ServiceStack.Redis;
+using ServiceStack.Text;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using StackExchange.Redis;
 
 namespace shreoyedis
 {
     class Program
     {
+        //static void Main(string[] args)
+        //{
+        //    using (var redis = new RedisClient())
+        //    {
+        //        var redisUsers = redis.As<Person>();
+        //        var rick = new Person { Id = redisUsers.GetNextSequence(), Name = "Rick" };
+        //        var becky = new Person { Id = redisUsers.GetNextSequence(), Name = "Becky" };
+
+        //        redisUsers.Store(rick);
+        //        redisUsers.Store(becky);
+        //        var allthePeople = redisUsers.GetAll();
+        //        Console.WriteLine(allthePeople.Dump());
+        //        Console.ReadLine();
+
+
+        //    }
+        //}
+
         static void Main(string[] args)
         {
+            ConnectionMultiplexer redisConn = ConnectionMultiplexer.Connect("localhost");
+            IDatabase redDb = redisConn.GetDatabase();
+            redDb.StringSet("Key1", "value1");
+            Console.WriteLine(redDb.StringGet("Key1"));
+            Console.Read();
         }
     }
 }
